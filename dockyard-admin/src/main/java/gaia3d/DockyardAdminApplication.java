06d8f7937e43c231120887d6f23f1c9b32fed3f2
hjchoi <hjchoi@gaia3d.com>
2019-01-04 19:03:41 +0900

    초기 프로젝트 생성

---
+++ dockyard-admin/src/main/java/gaia3d/DockyardAdminApplication.java
@@ -0,0 +1,14 @@
+package gaia3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockyardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockyardAdminApplication.class, args);
+	}
+
+}
+
---
+++ hmd-common/src/main/java/gaia3d/HmdCommonApplication.java
@@ -0,0 +1,14 @@
+package gaia3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmdCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmdCommonApplication.class, args);
+	}
+
+}
+
