25f36e4ba84e02480d9df0b362dd5119c258205d
hjchoi <hjchoi@gaia3d.com>
2019-01-09 14:01:33 +0900

    datasource, thymeleaf, lombok 설정

--- gis-admin/src/main/java/gaia3d/GeoAdminApplication.java
+++ gis-admin/src/main/java/gaia3d/GeoAdminApplication.java
@@ -2,9 +2,10 @@ package gaia3d;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GeoAdminApplication {
+public class GeoAdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoAdminApplication.class, args);
