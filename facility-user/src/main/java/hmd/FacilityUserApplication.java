7cf389b0b0a90e1d28c8023cd5c9ab2e68d54567
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2019-01-14 12:48:05 +0900

    merge

--- facility-user/src/main/java/hmd/FacilityUserApplication.java
+++ facility-user/src/main/java/hmd/FacilityUserApplication.java
@@ -2,12 +2,37 @@ package hmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+public class FacilityUserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilityUserApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FacilityUserApplication.class);
+	}
+	
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+//					.allowedHeaders("*");
+//			}
+//		};
+//	}
+	
+	// TODO 내장 된건가? spring.mvc.hiddenmethod.filter.enabled=false로 하고 활성화 가능
+//	@Bean
+//	public FilterRegistrationBean<HiddenHttpMethodFilter> hiddenHttpMethodFilter() {
+//		FilterRegistrationBean<HiddenHttpMethodFilter> registrationBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+//		registrationBean.addUrlPatterns("/*");
+//		return registrationBean;
+//	}
 }
-
