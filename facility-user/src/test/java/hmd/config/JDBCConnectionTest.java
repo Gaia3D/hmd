222670fb1e58306df7a231ea8303e85edbe44998
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2019-01-15 18:10:58 +0900

    삭제

--- facility-user/src/test/java/hmd/config/JDBCConnectionTest.java
+++ facility-user/src/test/java/hmd/config/JDBCConnectionTest.java
@@ -13,35 +13,11 @@ public class JDBCConnectionTest {
 
 	@Test
 	public void postgresqlConnection() {
-		try {
-			Class.forName("org.postgresql.Driver");
-			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mago3d", "postgres", "postgres");
-			PreparedStatement psmt = conn.prepareStatement("select 1");
-			ResultSet rs = null;
-			rs = psmt.executeQuery();
-			while (rs.next()) {
-				System.out.println("postgresql test code 성공 . value = " + rs.getString(1));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	@Test
 	public void oracleConnection() {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@118.216.255.110:15210:gaia3d11g", "hmd", "hmd");
-			PreparedStatement psmt = conn.prepareStatement("select 1 from dual");
-			ResultSet rs = null;
-			rs = psmt.executeQuery();
-			while (rs.next()) {
-				System.out.println("oracle test code 성공 . value = " + rs.getString(1));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 }
