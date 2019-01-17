74348c33eea2d03849e26117bb9f4e6ed4952ed9
shPark <shpark@gaia3d.com>
2019-01-17 18:47:19 +0900

    build.gradle 수정

--- facility-admin/src/main/java/hmd/FacilityAdminApplication.java
+++ facility-admin/src/main/java/hmd/FacilityAdminApplication.java
@@ -2,13 +2,19 @@ package hmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FacilityAdminApplication {
+public class FacilityAdminApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacilityAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FacilityAdminApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FacilityAdminApplication.class);
+    }
 }
 
