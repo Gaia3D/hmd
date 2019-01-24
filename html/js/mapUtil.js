365b8f092c762db4405c2a03bfdaa07d654751cc
kimhj <kimhj@gaia3d.com>
2019-01-24 17:05:14 +0900

    소스리뷰 보완사항 수정 1. parameter validation 2. for-in, foreach 브라우저 호환성 확인 3. 사용하는 변수명 변경
    수정 예정
    1. mapUtil의 파일명
    2. layerOnOff의 메소드명

--- html/js/mapUtil.js
+++ html/js/mapUtil.js
@@ -6,30 +6,36 @@ HMD.GIS.Util = (function() {
 		 * @returns {Object} Layer
 		 */
 		getLayerById: function(layerId) {
-			// valid
-			var layers = map.getLayers().getArray();
 			var layer = null;
 
-			for(var i in layers){	 // 브라우저확인
-				if(layers[i].get('id') === layerId){
-					layer = layers[i];
-					break;
+			if(layerId){
+				var layers = map.getLayers().getArray();
+
+				for(var i in layers){	 // 브라우저 호환성 - ie6~, chrome
+					if(layers[i].get('id') === layerId){
+						layer = layers[i];
+						break;
+					}
 				}
 			}
+
 			return layer;
 		},
 
 		/**
 		 * layer의 파라미터를 갱신하여 on/off를 상태를 변경한다.
-		 * @param {String} targetLayerId: 갱신할 레이어의 ID		(예) tile_layer, base_layer, ...
-		 * @param {String} showLayerNameList: 보여줄 레이어 이름	(예) ['layer_1', 'layer_2', ...]
+		 * @param {String} wmsLayerId: 			갱신할 레이어의 ID		(예) tile_layer, base_layer, ...
+		 * @param {String} sourceLayerIdList: 	소스 레이어 이름	리스트	(예) ['layer_1', 'layer_2', ...]
 		 */
 		// 이름바꾸기
-		layerOnOff: function(targetLayerId, showLayerNameList) {	 // wms
-			// valid
-			var targetLayer = this.getLayerById(targetLayerId);
-			targetLayer.getSource().updateParams({'layers': showLayerNameList});
-			targetLayer.setVisible(showLayerNameList.length !== 0);
+		layerOnOff: function(wmsLayerId, sourceLayerIdList) {
+			if(!wmsLayerId || !sourceLayerIdList){
+				return false;
+			}
+
+			var targetLayer = this.getLayerById(wmsLayerId);
+			targetLayer.getSource().updateParams({'layers': sourceLayerIdList});
+			targetLayer.setVisible(sourceLayerIdList.length !== 0);
 		}
 	}
 })();
