e493bc0c60ce02b8974ca4406939a097fe54d057
hjchoi <hjchoi@gaia3d.com>
2019-01-25 09:46:34 +0900

    설정 변경

--- gis-admin/src/main/java/hmd/config/ServletConfig.java
+++ gis-admin/src/main/java/hmd/config/ServletConfig.java
@@ -1,8 +1,5 @@
 package hmd.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -10,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +14,8 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -61,7 +55,7 @@ public class ServletConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		log.info(" @@@ ServletConfig addResourceHandlers @@@");
+		log.info(" @@@ ServletConfig addResourceHandlers @@@");
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 		registry.addResourceHandler("/externlib/**").addResourceLocations("/externlib/");
 		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
@@ -71,23 +65,5 @@ public class ServletConfig implements WebMvcConfigurer {
  
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
-	/*	@Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("forward:/index.jsp");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-    
-    @Bean
-	@ConditionalOnMissingBean(InternalResourceViewResolver.class)
-	public InternalResourceViewResolver viewResolver() {
-		log.info(" @@@ ServletConfig viewResolver @@@");
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views");
-		viewResolver.setSuffix(".jsp");
-		viewResolver.setOrder(3);
-		
-		return viewResolver;
-	}*/
 	
 }
